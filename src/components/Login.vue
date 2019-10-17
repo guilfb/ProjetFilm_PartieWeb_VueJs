cd9cc6db13ceef1479f5ebc1dcab23ede69daadc
guilfb <guillaumelefebvre69@gmail.com>
2019-10-17 10:46:32 +0200

    test2

---
+++ src/components/Login.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+
+    <h1>Welcome to the Movie App</h1>
+
+    <form>
+      <h3>Login</h3>
+      <input type="text"/>
+      <h3>Mot de passe</h3>
+      <input type="password"/>
+
+      <div class="button_submit">
+        <button type="submit">Valider</button>
+      </div>
+    </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+}
+</script>
+
+<style scoped>
+.button_submit {
+  padding: 20px;
+}
+</style>
+
